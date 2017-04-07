060bf9b4b9752d0e1daa88e45107052fc0bf9f51
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2017-04-07 15:14:55 +0800

    init

---
+++ ios/RCTTingYun/RCTTingYun.h
@@ -0,0 +1,13 @@
+//
+//  RCTTingYun.h
+//  RCTTingYun
+//
+//  Created by user on 2017/4/7.
+//  Copyright © 2017年 simple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTTingYun : NSObject
+
+@end
---
+++ ios/RCTTingYun/RCTTingYun.m
@@ -0,0 +1,13 @@
+//
+//  RCTTingYun.m
+//  RCTTingYun
+//
+//  Created by user on 2017/4/7.
+//  Copyright © 2017年 simple. All rights reserved.
+//
+
+#import "RCTTingYun.h"
+
+@implementation RCTTingYun
+
+@end
